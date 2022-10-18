d4fda40c7cc529f51cfd71d9993e730d24a72ab9
Bill Sion <bill@Bills-MacBook-Pro.local>
2022-10-18 11:58:08 +0800

    修改签名验证方法

--- app/Helpers/Sign.php
+++ app/Helpers/Sign.php
@@ -5,12 +5,14 @@ namespace App\Helpers;
 class Sign
 {
     /**
-     * Undocumented function.
+     * 加密方法.
      *
-     * @param string $api 接口名称
+     * @param string $api 接口全称 https://act.hbtv.com.cn/api/ip/api/{$ip}
      * @param array $params query params 参数
+     * @param string $app_key 公钥
+     * @param string $app_secret 密钥
      */
-    public static function gen(string $api, array $params, string $app_secret): string
+    public static function gen(string $api, array $params, string $app_key, string $app_secret): string
     {
         // 对 query params 参数进行 key 键排序，并生成待签名字符串
         if ($params) {
@@ -25,7 +27,7 @@ class Sign
         // post
         //$json_body = '';
         //$sign = $api + "\n" + $ksort_params + "\n" + base64_encode(md5($json_body));
-        $sign = base64_encode(hash_hmac('sha256', $str_to_sign, $app_secret));
+        $sign = base64_encode(hash_hmac('sha256', $str_to_sign, $app_key . $app_secret));
 
         return $sign;
     }
--- app/Http/Controllers/CkIPController.php
+++ app/Http/Controllers/CkIPController.php
@@ -26,15 +26,14 @@ class CkIPController extends Controller
      */
     public function check(Request $request): JsonResponse
     {
-        $sign    = base64_decode($request->headers->get('x-ca-signature'));
-        $app_key = $request->headers->get('x-ca-key');
-        $nonce   = $request->headers->get('x-ca-nonce');
+        $sign  = base64_decode($request->headers->get('x-ca-signature'));
+        $name  = $request->headers->get('x-ca-key');
+        $nonce = $request->headers->get('x-ca-nonce');
 
         // 判断 nonce 是否在有效期内
         // 如果请求的 nonce 不存在，设置 nonce
         if (! Cache::store('redis')->has($nonce)) {
             Cache::store('redis')->set($nonce, time(), env('NONCE_VALID_DURATION'));
-        // 作 IP 地址定位判断
         } else {
             return response()->json(['msg' => 'invalid nonce', 'code' => 'ok'])
             ->header('x-ca-error-message', 'invalid nonce');
@@ -44,13 +43,13 @@ class CkIPController extends Controller
         // 如果不存在，且请求时间有效则为合法请求，同时将 nonce 写入，并记录时间；如果不存在，且请求时间超出规定时限，判断为恶意请求。
         // 如果已经存在，判断为恶意请求。
 
-        if ($app = Auth::where('app_key', $app_key)->where('enabled', 1)->get()->toArray()) {
+        if ($app = Auth::where('name', $name)->where('enabled', 1)->get()->toArray()) {
             //$api = 'http://127.0.0.1:8092/api/127.0.0.1'; //不包括 query params
             $api = $_SERVER['APP_URL'] . $_SERVER['API'] . $request->route('ip');
             $request->merge(['ip' => $request->route('ip')]);
 
             // 计算当前的签名
-            $cal_sign = base64_decode(Sign::gen($api, request()->all(), $app[0]['app_secret']));
+            $cal_sign = base64_decode(Sign::gen($api, request()->all(), $app[0]['app_key'], $app[0]['app_secret']));
             // 对签名进行比较
             if ($sign == $cal_sign) {
                 $city = new City(storage_path('/app/ipipfree.ipdb'));
@@ -73,7 +72,7 @@ class CkIPController extends Controller
      */
     public function gen(Request $request): JsonResponse
     {
-        $auth = Auth::find(1)->toArray();
+        $auth = Auth::where('name', 'abc')->first()->toArray();
         if ($ip = $request->get('ip', '')) {
             //计算 nonce
             //$api = 'http://127.0.0.1:8093/api/' . $ip; //不包括 query params
@@ -81,10 +80,10 @@ class CkIPController extends Controller
             // app key + api + nonce
             $nonce = UUID::v3(md5($auth['app_key'] . $api . time()), Auth::rand_chars());
 
-            $sign = Sign::gen($api, request()->all(), $auth['app_secret']);
+            $sign = Sign::gen($api, request()->all(), $auth['app_key'], $auth['app_secret']);
 
             $headers = [
-                'x-ca-key'               => $auth['app_key'],
+                'x-ca-key'               => $auth['name'],
                 'x-ca-signature-method'  => 'HmacSHA256',
                 'x-ca-signature'         => $sign,
                 'x-ca-timestamp'         => time(),
